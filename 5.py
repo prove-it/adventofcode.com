c81ca67cce2415040e434bc3823370d19c3abf7d
Shevchenko Ekaterina <eshevchenko8@gmail.com>
2016-03-02 21:27:28 +0400

    5th puzzle: 1st part

---
+++ 5.py
@@ -0,0 +1,43 @@
+import re
+
+def count_of_chars(str, chars):
+    regexp = r'[' + chars + ']'
+
+    str_vowels = re.findall(regexp, str)
+
+    return len(str_vowels)
+
+def has_repetitions(str):
+    regexp = r'(\w)\1+'
+
+    str_repetitions = re.search(regexp, str)
+
+    return bool(str_repetitions)
+
+def has_one_of_subs(str, subs):
+    regexp = r'(' + '|'.join(subs) + ')'
+
+    subs_in_str = re.search(regexp, str)
+
+    return bool(subs_in_str)
+
+def is_nice_string(str):
+    exceptions = ['ab', 'cd', 'pq', 'xy']
+    vowels = 'aeiou'
+
+    if count_of_chars(str, vowels) >= 3 and has_repetitions(str) and not has_one_of_subs(str, exceptions):
+        return True
+    else:
+        return False
+
+def is_nice_string_v2(str):
+    pass
+
+def count_nice_strings(str_list, cheking_func):
+    return sum(1 for str in str_list if cheking_func(str))
+
+strings_f = open('inputs/5.txt', 'r')
+strings = strings_f.read()
+strings_f.close()
+
+print count_nice_strings(strings.split('\n'), is_nice_string)
