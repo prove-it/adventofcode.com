b77eb6bcc03374198d3807aa0392090df2711b2f
Ekaterina Shevchenko <e.shevchenko@abbyy-ls.com>
2016-03-26 16:39:40 +0400

    7th puzzle: dynamic programming

--- 7.py
+++ 7.py
@@ -6,45 +6,34 @@ class Circuit:
 
     def __init__(self, instruction):
         self.inst = instruction
+        self._wires = {}
+        self.signals = {}
 
     @staticmethod
     def _replace_text(repl_dict, text):
         pattern = r'\b' + r'\b|\b'.join(repl_dict.keys()) + r'\b'
         return re.sub(pattern, lambda m: str(repl_dict[m.group(0)]), text)
 
-    def _get_computable_wires(self, text):
-        pattern = r'^((?:[\-\d]*)?\s?(?:' + '|'.join(self.BITWISE_DICT.keys()) + r')?\s?(?:[\-\d]*))\s\->\s([a-z]+)$'
-        return {wire: value for value, wire in re.findall(pattern, text, re.MULTILINE)}
+    def compute_signal(self, wire):
+        if wire.isdigit():
+            return int(wire)
+        else:
+            if wire not in self.signals:
+                self.signals[wire] = eval(self._wires[wire])
+            return self.signals[wire]
 
-    def _replace_bitwise_ops(self, text):
-        return self._replace_text(self.BITWISE_DICT, text)
-
-    def _replace_computed_wires(self, wires, text):
-        return self._replace_text(wires, text)
-
-    def override_wire(self, wire, new_expr):
-        pattern = r'(.*)(?=\s\->\s\b' + wire + r'\b)'
-        self.inst = re.sub(pattern, str(new_expr), self.inst)
-
-    def find_wire_signal(self, searched_wire):
-        inst = self.inst
-
-        while True:
-            cur_wires = self._get_computable_wires(inst)
-
-            for wire, expr in cur_wires.iteritems():
-                cur_wires[wire] = eval(self._replace_bitwise_ops(expr))
-                if wire == searched_wire:
-                    return cur_wires[wire]
-
-            inst = self._replace_computed_wires(cur_wires, inst)
-
+    def set_wires(self):
+        for one_inst in self.inst.split('\n'):
+            expr, wire = re.split(r'\s\->\s', one_inst)
+            expr = self._replace_text(self.BITWISE_DICT, expr)
+            self._wires[wire] = re.sub('([a-z]+)', lambda m: 'self.compute_signal("' + m.group(0) + '")', expr)
 
 instructions_text = get_file_content('7.txt')
 circuit = Circuit(instructions_text)
+circuit.set_wires()
 
-a = circuit.find_wire_signal('a')
+a = circuit.compute_signal('a')
 print a
 
-circuit.override_wire('b', a)
-print circuit.find_wire_signal('a')
+circuit.signals = {'b': a}
+print circuit.compute_signal('a')
