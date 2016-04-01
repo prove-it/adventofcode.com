b87a2baf3ccfa5777f5e29dba87971a13c73c3a5
Ekaterina Shevchenko <e.shevchenko@abbyy-ls.com>
2016-04-01 23:52:17 +0400

    10th puzzle: deleted unnecessary str call

--- 10.py
+++ 10.py
@@ -3,7 +3,7 @@ import re
 def look_and_say(number, iterations):
     for i in xrange(iterations):
         pattern = r'(\d)\1*'
-        number = re.sub(pattern, lambda m: str(len(m.group(0))) + str(m.group(0)[0]), number)
+        number = re.sub(pattern, lambda m: str(len(m.group(0))) + m.group(0)[0], number)
 
     return number
 
